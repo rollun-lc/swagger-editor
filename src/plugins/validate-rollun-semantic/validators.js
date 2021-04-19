41ae02ed8f45951c5bc040a08350e961abe612ae
artemskii-dev <artem.dovhopolyi.dev@gmail.com>
2021-04-19 18:40:10 +0300

    fixed a bug with url with slash

--- src/plugins/validate-rollun-semantic/validators.js
+++ src/plugins/validate-rollun-semantic/validators.js
@@ -104,7 +104,7 @@ export const validateServers = (JSONSpec) => ({errActions, specSelectors, fn: {A
     })
   }
 
-  const regex = new RegExp(`^https?://[a-z][a-z0-9]{0,14}/openapi/${JSONSpec.info.title || "EmptyTitle"}/v${JSONSpec.info.version}$`)
+  const regex = new RegExp(`^https?://[a-z][a-z0-9-]{0,14}/openapi/${JSONSpec.info.title || "EmptyTitle"}/v${JSONSpec.info.version}$`)
   const invalidTagIndex = JSONSpec.servers.findIndex(({url}) => {
     return !regex.test(url)
   })
