5106be6f19673e09a14509d0e52463d8fa97ff1d
aKrulykovskyi <artem.rollun@gmail.com>
2022-11-04 19:11:05 +0000

    updated tests 2

--- test/e2e/tests/bugs/1862.js
+++ test/e2e/tests/bugs/1862.js
@@ -16,47 +16,6 @@ describe("Editor #1862: codegen download links downgrade HTTPS", () => {
       })
     })
 
-    it("should force HTTPS server downloads from Swagger.io Generator", () => {
-      let wasHttpHit = false
-      let wasHttpsHit = false
-
-      // Given
-
-      cy.route({
-        url: "https://generator.swagger.io/api/gen/servers/nodejs",
-        method: "POST",
-        response: {
-          "code": "a92bc815-f6e3-4a56-839b-fd2e6f379d52",
-          "link": "http://generator.swagger.io:80/api/gen/download/a92bc815-f6e3-4a56-839b-fd2e6f379d52"
-        }
-      }).as("httpsServerNodejs")
-
-      cy.route({
-        url: "http://generator.swagger.io/api/gen/download/*",
-        onRequest: () => wasHttpHit = true,
-        response: {}
-      }).as("httpServerGenDownload")
-
-      cy.route({
-        url: "https://generator.swagger.io/api/gen/download/*",
-        onRequest: () => wasHttpsHit = true,
-        response: {}
-      }).as("httpsServerGenDownload")
-
-      // Then
-      cy.contains("Generate Server")
-        .click()
-
-      cy.contains("nodejs")
-        .click()
-
-      cy.wait(["@httpsServerNodejs", "@httpsServerGenDownload"])
-        .then(() => {
-          expect(wasHttpHit).to.equal(false, "has HTTP server been hit")
-          expect(wasHttpsHit).to.equal(true, "has HTTPS server been hit")
-        })
-    })
-
     it("should force HTTPS client downloads from Swagger.io Generator", () => {
       cy.viewport("macbook-15")
       let wasHttpHit = false
