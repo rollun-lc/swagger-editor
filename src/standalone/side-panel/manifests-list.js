5d830939050d645c291008865143876051c264fe
kostrov-rollun <kostrov.rollun@gmail.com>
2023-06-16 11:07:18 +0000

    add .yaml file extension to filter

--- src/standalone/side-panel/manifests-list.js
+++ src/standalone/side-panel/manifests-list.js
@@ -59,7 +59,7 @@ export default class ManifestsList extends React.Component {
       }
     })
 
-    const filterFiles = (tree) => tree.filter(({type, path}) => type === "blob" && _.endsWith(path, ".yml"))
+    const filterFiles = (tree) => tree.filter(({type, path}) => type === "blob" && (_.endsWith(path, ".yml") || _.endsWith(path, ".yaml")))
 
     let branchesTrees = {}
     for (const {name} of branches) {
