4b7e7ee3592da0b1693ecec9c7469be24b75457b
artemka-debug <dovgopoly123@gmail.com>
2023-09-18 17:08:31 +0300

    feat: add ignore of rollun semantic errors

--- src/plugins/validate-rollun-semantic/index.js
+++ src/plugins/validate-rollun-semantic/index.js
@@ -9,24 +9,24 @@ import {updateJsonSpec, updateSpec} from "./actions"
 
 export const ROLLUN_SEMANTIC_ERROR_PREFIX = "rollun-semantic"
 
-const urlParams = new URLSearchParams(window.location.search);
-const ignoreRollunSemanticErrors = urlParams.get('ignoreRollunSemanticErrors');
-
-console.log(urlParams, ignoreRollunSemanticErrors);
-
-const actions = ignoreRollunSemanticErrors === 'true' ? {} : {
-  validateForbiddenKeys,
-  validateTags,
-  validateVersion,
-  validateTitle,
-  validateServers
-};
-const wrapActions = ignoreRollunSemanticErrors === 'true' ? {} : {
-  updateSpec,
-  updateJsonSpec
-};
-
 export default function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const ignoreRollunSemanticErrors = urlParams.get('ignoreRollunSemanticErrors');
+
+  console.log(urlParams, ignoreRollunSemanticErrors);
+
+  const actions = ignoreRollunSemanticErrors === 'true' ? {} : {
+    validateForbiddenKeys,
+    validateTags,
+    validateVersion,
+    validateTitle,
+    validateServers
+  };
+  const wrapActions = ignoreRollunSemanticErrors === 'true' ? {} : {
+    updateSpec,
+    updateJsonSpec
+  };
+
   return {
     statePlugins: {
       spec: {
